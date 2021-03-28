ceedc2ca9865b3ea7fd39179a6337aa0838d2466
LUOYIXIN213 <luoyixin213@gmail.com>
2021-03-28 17:16:49 +0800

    Create routes.js

---
+++ bt3103_finalproj/src/routes.js
@@ -0,0 +1,19 @@
+//Step 2 -- Define Routes
+import Home from './components/Home.vue'
+import Profile from './components/Profile.vue'
+import Leaderboard from './components/Leaderboard.vue'
+import Rewards from './components/Rewards.vue'
+import Newsroom from './components/Newsroom.vue'
+import PastTransactions from './components/PastTransactions.vue'
+import Help from './components/Help.vue'
+import Logout from './components/Logout.vue'
+export default[
+    { path: '/', component: Home},
+    { path: '/profile', component: Profile},
+    { path: '/leaderboard', component:Leaderboard},
+    { path: '/rewards', component:Rewards},
+    { path: '/newsroom', component:Newsroom},
+    { path: '/past', component:PastTransactions},
+    { path: '/help', component:Help},
+    { path: '/logout', componet:Logout}   
+]
