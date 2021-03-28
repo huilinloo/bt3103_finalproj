422a12f088cc5e743e1a21627a6262b32b41da76
Hui Lin <huilinloo@Huis-MacBook-Air-2.local>
2021-03-28 16:42:21 +0800

    vue

---
+++ bt3103_finalproj/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link to="/" exact>Home</router-link>
+      </li>
+      <li>
+        <router-link to="/profile" exact>Profile</router-link>
+      </li>
+      <li>
+        <router-link to="/leaderboard" exact>Leaderboard</router-link>
+      </li>
+      <li>
+        <router-link to="/rewards" exact>Rewards</router-link>
+      </li>
+      <img src = "logo greensg.PNG"></img>
+      <li>
+        <router-link to="/newsroom" exact>Newsroom</router-link>
+      </li>
+      <li>
+        <router-link to="/past" exact>Past Transactions</router-link>
+      </li>
+      <li>
+        <router-link to="/help" exact>Help</router-link>
+      </li>
+      <li>
+        <router-link to="/logout" exact>Log Out</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
