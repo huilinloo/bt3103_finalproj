e3ca01925de1ad2b65099a9fd7330b8e832ab1e9
Hui Lin <loo.huilin@u.nus.edu>
2021-03-29 19:59:08 +0800

    completed help.vue

--- bt3103_finalproj/src/components/Help.vue
+++ bt3103_finalproj/src/components/Help.vue
@@ -1,8 +1,45 @@
 <template>
 <div id="help">
     <h1>FAQs</h1>
-    <p id="question 1">
-        How do I update my profile?
+    <h2 id="question 1">
+        How do I update my profile?<br>
+    </h2>
+    <p id="answer 1">
+        Please follow the instructions below:<br>
+        a) Tap on "Profile" on the menu bar of the website.<br>
+        b) Tap "Edit my profile" at the top of the page.<br>
+        c) Click "Save profile" after making the necessary changes.<br>
     </p>
+    <h2 id="question 2">
+        How do I redeem the points I have accumulated?<br>
+    </h2>
+    <p id="answer 2">
+        Please follow the instructions below:<br>
+        a) Tap on "Rewards" on the menu bar of the website.<br>
+        b) Tap "Redeem points" button.<br>
+        c) Choose which vouchers/coupons you would like to redeem.<br>
+        d) Under the "Redeemed" tab you can now see your voucher/coupon code.
+        Show this code to the merchant on your next purchase.
+    </p>
+    <h2 id="question 3">
+        Do points expire?<br>
+    </h2>
+    <p id="answer 3">
+        No, points do not expire. However, vouchers have a validation period of 4 weeks.
+    </p>
+    
 </div>
 </template>
+
+<style scoped>
+h1 {
+    font-size: 80px;
+}
+h2 {
+    font-size: 50px;
+    text-decoration:underline;
+}
+p {
+    font-size: 25px;
+}
+</style>
