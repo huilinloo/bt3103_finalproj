b5d7e6132aae5786b5fba7b44fe6e428e201a28a
Xinyi Lu <xinyilu2000@gmail.com>
2021-04-04 22:56:59 +0800

    changed ui for leaderboard

--- bt3103_finalproj/src/components/Leaderboard.vue
+++ bt3103_finalproj/src/components/Leaderboard.vue
@@ -1,37 +1,42 @@
 <template>
     <div id = "leaderboard">
+        <div id = "header">
+            <img src = "https://www.freeiconspng.com/thumbs/leaderboard-icon/leaderboard-icon-9.png" id = "trophy">
+            <h1> Leaderboard </h1>
+            <p><i> Save more plastic disposables to climb up the leaderboard! </i></p>
+        </div>
         <div class = "sg">
-            <p id = "textsg">Singapore</p>
-            <p class = "description" >Points earned today</p>
+            <p id = "textsg">Top 10 <br><br><br> Singapore</p>
+            <!--<p class = "description" >Points earned today</p>-->
             <br><br><br>
             <table>
                 <tr>
-                    <th>Position</th>
-                    <th>User</th>
-                    <th>Points</th>
+                    <th><img src = "https://image.flaticon.com/icons/png/512/15/15643.png" class = "rank_img"> Rank</th>
+                    <th><img src = "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png" class = "user_img"> User</th>
+                    <th><img src = "https://cdn4.iconfinder.com/data/icons/plastic-bag/100/plastic_bag-06-512.png" class = "plastic_img"> Total Plastic Saved</th>
                 </tr>
                 <tr v-for="(user, index) in sortArrays(sg)" :key = "index">
                     <td class = "position"><b>{{index+1}}</b></td>
                     <td>{{user.name}}</td>
-                    <td>{{user.points}}</td>
+                    <td>{{user.total_plastic}}</td>
                 </tr>
             </table>
         </div>
         <div class = "vl"></div>
         <div class = "friends">
-            <p id = "textfriends">Friends</p>
-            <p class = "description">Points earned today</p>
+            <p id = "textfriends">Top 10 <br><br><br> Friends</p>
+            <!--<p class = "description">Total plastic saved</p>-->
             <br><br><br>
             <table>
                 <tr>
-                    <th>Position</th>
-                    <th>User</th>
-                    <th>Points</th>
+                    <th><img src = "https://image.flaticon.com/icons/png/512/15/15643.png" class = "rank_img"> Rank</th>
+                    <th><img src = "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png" class = "user_img"> User</th>
+                    <th><img src = "https://cdn4.iconfinder.com/data/icons/plastic-bag/100/plastic_bag-06-512.png" class = "plastic_img"> Total Plastic Saved</th>
                 </tr>
                 <tr v-for="(user, index) in sortArrays(friends)" :key = "index">
                     <td class = "position"><b>{{index+1}}</b></td>
                     <td>{{user.name}}</td>
-                    <td>{{user.points}}</td>
+                    <td>{{user.total_plastic}}</td>
                 </tr>
             </table>
         </div>
@@ -50,21 +55,13 @@ export default {
     },
     methods: {
         fetchData: function() {
-            /*
-            database.collection('leaderboard').doc('march_29').get().then(snapshot => {
-                snapshot.docs.forEach(doc => {
-                    var date = snapshot.data();
-                    console.log(date);                   
-
-                })
-            */
-            database.collection('leaderboard').doc('march_29').collection('friends').get().then(snapshot =>
+            database.collection('leaderboard').doc('qalKACH2NQF9vxhOrCDL').collection('friends').get().then(snapshot =>
                 snapshot.docs.forEach(doc => {
                     var friend = doc.data()
                     console.log(friend)
                     this.friends.push(friend)
                 }));
-            database.collection('leaderboard').doc('march_29').collection('singapore').get().then(snapshot => 
+            database.collection('leaderboard').doc('qalKACH2NQF9vxhOrCDL').collection('singapore').get().then(snapshot => 
                 snapshot.docs.forEach(doc => {
                     var user = doc.data()
                     console.log(user)
@@ -72,7 +69,7 @@ export default {
                 }));
         },
         sortArrays: function(arrays) {
-            return arrays.sort((a, b) => b.points - a.points);
+            return arrays.sort((a, b) => b.total_plastic - a.total_plastic);
         }
     },
     mounted() {
@@ -82,9 +79,32 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+    font-size: 80px;
+}
+
 .sg {
     float: left;
     width: 49%;
+}
+
+#trophy {
+    vertical-align: middle;
+}
+
+#header > h1 {
+    vertical-align: middle;
+    display: inline;
+}
+
+#header > p {
+    position: relative;
+    font-size: 30px;
+    right: -200px;
+}
+.rank_img, .user_img, .plastic_img {
+    width: 30px;
+    height: 30px;
 }
 
 #textfriends, #textsg {
@@ -95,9 +115,10 @@ export default {
     line-height: 26.4px;
 }
 
+/*
 .description {
     font-family: Avenir, Helvetica, Arial, sans-serif;
-}
+}*/
 
 .vl {
     float: left;
@@ -118,7 +139,7 @@ table {
     width: 700px;
     //background: #90EE90;
     border: 1px solid black;
-    font-size: 28px;
+    font-size: 36px;
 }
 
 th{
@@ -127,8 +148,9 @@ th{
 }
 
 td {
-    padding-top: 20px;
-    padding-bottom: 20px;
+    //padding-top: 20px;
+    //padding-bottom: 20px;
+    padding:10px 25px 10px 25px;
 }
 
 .position {
