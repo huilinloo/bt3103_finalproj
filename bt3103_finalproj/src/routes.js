7e91730659d5b4df01d10be530511de6bb24b6a2
Xinyi Lu <xinyilu2000@gmail.com>
2021-04-04 17:53:23 +0800

    update routes.js

--- bt3103_finalproj/src/routes.js
+++ bt3103_finalproj/src/routes.js
@@ -1,12 +1,12 @@
 //Step 2 -- Define Routes
-//import Home from './components/Home.vue'
-//import Profile from './components/Profile.vue'
+import Home from './components/Home.vue'
+import Profile from './components/Profile.vue'
 import Leaderboard from './components/Leaderboard.vue'
-//import Rewards from './components/Rewards.vue'
+import Rewards from './components/Rewards.vue'
 import Newsroom from './components/Newsroom.vue'
-//import PastTransactions from './components/PastTransactions.vue'
+import PastTransactions from './components/PastTransactions.vue'
 import Help from './components/Help.vue'
-//import Logout from './components/Logout.vue'
+import Logout from './components/Logout.vue'
 export default[
     //{ path: '/', component: Home},
     //{ path: '/profile', component: Profile},
