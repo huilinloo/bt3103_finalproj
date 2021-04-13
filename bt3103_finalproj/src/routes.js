67359c91d1d6fb0be3e18d9b3d3781c5a123a017
Xinyi Lu <xinyilu2000@gmail.com>
2021-04-13 12:00:36 +0800

    fixed hiding header in login, added routes for login-all

--- bt3103_finalproj/src/routes.js
+++ bt3103_finalproj/src/routes.js
@@ -1,4 +1,5 @@
 //Step 2 -- Define Routes
+import Loginall from './components/Login-all.vue'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Profile from './components/Profile.vue'
@@ -7,14 +8,13 @@ import Rewards from './components/Rewards.vue'
 import Newsroom from './components/Newsroom.vue'
 import History from './components/History.vue'
 import Help from './components/Help.vue'
-//import Logout from './components/Logout.vue'
 import Signup from './components/Signup.vue'
 import Forgotpassword from './components/Forgot-password.vue'
 import Merchantlogin from './components/Merchant-login.vue'
 import Merchantinputs from './components/Merchant-inputs.vue'
 
 export default[
-    { path: '/', component: Login},
+    { path: '/', component: Loginall},
     { path: '/home', component: Home},
     { path: '/profile', component: Profile},
     { path: '/leaderboard', component:Leaderboard},
@@ -22,7 +22,7 @@ export default[
     { path: '/newsroom', component:Newsroom},
     { path: '/history', component:History},
     { path: '/help', component:Help},
-    //{ path: '/logout', componet:Logout}
+    { path: '/login', component:Login},
     { path: '/signup', component:Signup},
     { path: '/forgot-password', component:Forgotpassword},
     { path: '/merchant-login', component:Merchantlogin},
