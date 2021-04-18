8d511a55a49f7cfa8b5bc0414211c5f2e3ff6d9d
Xinyi Lu <xinyilu2000@gmail.com>
2021-04-18 11:04:53 +0800

    added input field for diff plastic categories

--- bt3103_finalproj/src/components/Merchant-inputs.vue
+++ bt3103_finalproj/src/components/Merchant-inputs.vue
@@ -4,39 +4,42 @@
     <form ref= "anyName" action="#" @submit.prevent="addPoints">
 	<div class="input_container">
         <h3><Strong>Add Customer Points</Strong></h3><br>
-        <label for="phone">Phone Number: </label><br>
-        <input type="text" id="phone" class="form-control form-control-lg" v-model="phone" required>
-        <p v-if="phone != ''">{{"Username is " + this.username}}</p>
+        <label style = "font-size: 20px;" for="phone">Phone Number: </label><br>
+        <input type="text" id="phone" class="form-control form-control-lg" v-model.lazy="phone" required>
+        <p v-if="checkPhone() == true">{{"Username is " + this.username}}</p>
         <br><br>
-        <label for="points">Points Earned: </label><br>
-        <input type="number" class="form-control form-control-lg" v-model.number="new_points" v-on:click="checkPhone()" required/>
+        <label style = "font-size: 20px;" for="points">Points Earned: </label><br>
+        <!--<input type="number" class="form-control form-control-lg" v-model.number.lazy="new_points" v-on:click="checkPhone()" required/>-->
         <br>
 
-        <p id = "allocation"><b>Information on points allocation</b></p>
+        <!--<p id = "allocation"><b>Information on points allocation</b></p>-->
+        <form id = "my_form"></form>
         <table id = "info">
           <tr>
             <th>Item</th>
+            <th>Amount of Points per item</th>
             <th>Quantity</th>
-            <th>Amount of Points</th>
           </tr>
           <tr>
             <td>Straw</td>
-            <td>1</td>
             <td>5</td>
+            <td><input min = "0" type = "number" v-model.number.lazy = "straw_qty" style = "width: 50px;height = 20px" required></td>
           </tr>
           <tr>
             <td>Plastic Bag</td>
-            <td>1</td>
             <td>10</td>
+            <td><input min = "0" type = "number" v-model.number.lazy = "pb_qty" style = "width: 50px" required></td>
           </tr>
           <tr>
             <td>Plastic Container</td>
-            <td>1</td>
             <td>20</td>
+            <td><input min = "0" type = "number" v-model.number.lazy = "pc_qty" style = "width: 50px" required></td>
           </tr>
         </table>
         <br>
-        <button type="submit" class="btn btn-success btn-lg btn-block" v-on:click="addPoints()">Submit</button>
+        <span style = "font-size: 20px;"> Total points: {{calc_total}}</span>
+        <br>
+        <button type="submit" class="btn btn-success btn-lg btn-block" >Submit</button>
         <br>
   </div>
     </form>
@@ -45,6 +48,7 @@
 
 <script>
 import database from '../firebase.js'
+//import firebase from 'firebase/app'
 
 export default {
     data(){
@@ -56,8 +60,21 @@ export default {
             new_points: 0,
             id: "",
             curr_points: null,
-            curr_plastic: null
+            curr_plastic: null,
+            date: null,
+            list_plastic: null,
+            straw_qty: 0,
+            pb_qty: 0,
+            pc_qty: 0,
+            updated_points: 0,
+            updated_plastic: 0,
+            plastic_cat: null
         }
+    },
+    computed: {
+      calc_total: function() {
+        return this.straw_qty*5 + this.pb_qty*10 + this.pc_qty*20
+      }
     },
     methods:{
         fetchItem:function(){
@@ -79,40 +96,79 @@ export default {
                 this.username = user[1].username
                 this.id = user[0]
                 //console.log(this.id)
-                this.curr_points = user[1].points
-                this.curr_plastic = user[1].totalplastic
-                //console.log(this.curr_points)
-                //console.log(typeof this.curr_points)
             }
           }
           if (this.sameNumber == false) {
               this.username = "not found"
+              return false
           }
+          return true
         },
         addPoints: function() {
             if (this.sameNumber == false) {
                 alert("Invalid phone number!");
                 this.$router.push('/merchant-inputs') //why cannot?
             } else {
-                //straw: 5g, 5 points
-                //plastic bag: 10g, 10 points
-                //plastic container: 20g, 20 points
-                var updated_points = this.curr_points + this.new_points
-                //console.log(this.new_points)
-                //console.log(typeof updated_points)
+                this.new_points = this.calc_total
+                console.log(this.new_points)
+                for (var i = 0; i < this.users.length; i++) {
+                  var user = this.users[i]
+                  if (user[1].phone == this.phone) {
+                  this.curr_points = user[1].points
+                  this.curr_plastic = user[1].totalplastic
+                  this.list_plastic = user[1].list_plastic
+                  this.plastic_cat = user[1].plastic_cat
+                  }
+                }
+
+                console.log("list of plastic" + this.list_plastic)
+                console.log("current amount of points" + this.curr_points)
+                console.log("type of curr_points" + typeof this.curr_points)
+
+                //update points and plastic field
+                this.updated_points = this.curr_points + this.new_points
+                this.updated_plastic = this.curr_plastic + this.new_points
+                console.log("new points" + this.new_points)
+                console.log("updated amt of points" + this.updated_points)
+
+                //if user makes 2nd purchase on the same day, this.date should exist in this.list_plastic object already
+                if (this.date in this.list_plastic) {
+                  this.list_plastic[this.date] = this.list_plastic[this.date] + this.new_points
+                } 
+
+                //if user makes 1st purchase today, add this.date key and this.new_points field
+                else {
+                  this.list_plastic[this.date] = this.new_points
+                }
+
+                //update by plastic category
+                this.plastic_cat['straw'] = this.plastic_cat['straw'] + this.straw_qty * 5
+                this.plastic_cat['plastic_bag'] = this.plastic_cat['plastic_bag'] + this.pb_qty * 10 
+                this.plastic_cat['plastic_container'] = this.plastic_cat['plastic_container'] + this.pc_qty * 20
+
+                console.log("updated list of plastic" + this.list_plastic)
+
                 database.collection("users").doc(this.id).update({
-                    points: updated_points,
-                    totalplastic: this.curr_plastic + this.new_points
+                    points: this.updated_points,
+                    totalplastic: this.updated_plastic,
+                    list_plastic: this.list_plastic,
+                    plastic_cat: this.plastic_cat
                 });
                 alert("Successfully added points!");
-                this.username = '';
-                this.new_points = 0;
-                this.$refs.anyName.reset();
+                //location.reload();
+                //this.username = '';
+                //this.new_points = null;
+                //this.$refs.anyName.reset();
             }
+        },
+        getTodaysDate: function() {
+          var today = new Date();
+          this.date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
         }
     },
     created() {
         this.fetchItem()
+        this.getTodaysDate()
     } 
 }
 </script>
